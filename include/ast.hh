cb0cf8efc20455cb5820dcb3253a0dad67153617
bulat1337 <bulatmot@gmail.com>
2025-02-22 21:02:29 +0300

    construct prototype

--- include/ast.hh
+++ include/ast.hh
@@ -18,14 +18,12 @@ private:
     using VarTable = std::unordered_map<std::string, int>;
 
 public:
-    std::unique_ptr<ScopeNode> globalScope;
+	ScopeNode* globalScope;
 
+	std::vector<std::unique_ptr<INode>> data_;
+	
 private:
-
-    std::vector<VarTable> VarTables_;
-
     detail::Context ctx;
-
 
 public:
 	AST(std::ostream& out = std::cout):
@@ -36,6 +34,18 @@ public:
 		MSG("Evaluating global scope\n");
         globalScope->eval(ctx);
     }
+
+	template <typename NodeType, typename... Args>
+	inline NodeType* construct(Args&&... args)
+	{
+		auto node_ptr = std::make_unique<NodeType>(std::forward<Args>(args)...);
+
+		auto raw_data = node_ptr.get();
+
+		data_.emplace_back(node_ptr);
+
+		return raw_data;
+	}
 };
 
 } // namespace AST
